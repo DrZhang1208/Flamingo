c43bd92fff05983c1113237f2b18d5fe748282de
Zhangdd1208 <zhangdd1208@foxmail.com>
2026-05-26 16:33:05 +0800

    修复歌词

--- app/src/main/java/yos/music/player/BaseActivity.kt
+++ app/src/main/java/yos/music/player/BaseActivity.kt
@@ -1,5 +1,6 @@
 package yos.music.player
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.core.view.WindowCompat
@@ -8,5 +9,17 @@ abstract class BaseActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
+        enableHighRefreshRate()
+    }
+
+    private fun enableHighRefreshRate() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val display = windowManager.defaultDisplay
+            val modes = display?.supportedModes ?: return
+            val maxRefreshRate = modes.maxOfOrNull { it.refreshRate } ?: return
+            window.attributes = window.attributes.apply {
+                preferredRefreshRate = maxRefreshRate
+            }
+        }
     }
 }
